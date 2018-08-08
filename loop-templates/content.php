c1e2f01d2fbc3b3f8b1044f267c63258ee094bbd
Tim Smith <tgsmith489@gmail.com>
2018-08-08 00:19:45 -0500

    Set up posts page and single post page

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -7,44 +7,22 @@
 
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php post_class('border-bottom pt-5 pb-4'); ?> id="post-<?php the_ID(); ?>">
+	<div class="post-image">
+		<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+	</div>
 
-	<header class="entry-header">
+	<div class="entry-content pt-3">
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-		'</a></h2>' ); ?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-
-			<div class="entry-meta">
-				<?php understrap_posted_on(); ?>
-			</div><!-- .entry-meta -->
-
-		<?php endif; ?>
-
-	</header><!-- .entry-header -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
-	<div class="entry-content">
+	<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+'</a></h2>' ); ?>
 
 		<?php
 		the_excerpt();
 		?>
 
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
-		?>
+		
 
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-
-		<?php understrap_entry_footer(); ?>
-
-	</footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
